9213b5419f01992f14b6b999f344c90f3cd9a83c
Kirill09921 <kirilldrachkirill@gmail.com>
2025-06-24 09:20:14 +0300

    PP-0000 - Initial commit

---
+++ src/test/java/edu/grsu/practice/price/PriceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.grsu.practice.price;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
