81e0a9fadf31497244657a19f973d227684f68ca
Kirill09921 <kirilldrachkirill@gmail.com>
2025-06-24 12:07:54 +0300

    PP-0001 - Fixed Db connection and Mapper

--- src/main/java/edu/grsu/practice/price/mapper/PriceMapper.java
+++ src/main/java/edu/grsu/practice/price/mapper/PriceMapper.java
@@ -3,9 +3,14 @@ package edu.grsu.practice.price.mapper;
 import edu.grsu.practice.price.dto.PriceDto;
 import edu.grsu.practice.price.model.Price;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface PriceMapper {
+
+    @Mapping(source = "id.departureLocation", target = "departureLocation")
+    @Mapping(source = "id.arrivalLocation", target = "arrivalLocation")
     PriceDto toDto (Price price);
+
 }
